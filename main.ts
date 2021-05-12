3b878518f7093a935797e0aa528cb71ed714ff1e
sintzes <82532147+sintzes@users.noreply.github.com>
2021-05-12 12:31:59 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 function Script () {
-    game.showLongText("Lisa: *yawn* If you're borrowing a book, please register it in the log. The restricted section is currently not lending any books...", DialogLayout.Bottom)
+    game.showLongText("Lisa: *yawn* If you're borrowing a book, please register it in the log.", DialogLayout.Bottom)
     game.showLongText("Lisa: Oh? It's you, little cutie.", DialogLayout.Bottom)
     game.showLongText("Lisa: It's so good to see you! More refreshing than a strong cup of tea in the afternoon.", DialogLayout.Bottom)
     game.showLongText("Y/N: Clearly you're meant to be working, but you don't seem to have your heart in it at all...", DialogLayout.Bottom)
